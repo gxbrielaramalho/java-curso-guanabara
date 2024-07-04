f623368e24bc20f099898de0557a224bd4841910
Gabriela Ramalho <gabrielathecreator@gmail.com>
2024-07-04 19:47:54 -0300

    Update SaidaDeDados.java

--- SaidaDeDados/SaidaDeDados.java
+++ SaidaDeDados/SaidaDeDados.java
@@ -1,9 +1,18 @@
 package SaidaDeDados;
+import java.util.Scanner;
 
 public class SaidaDeDados {
     public static void main (String [] args) {
-        String nome = "Gabriela";
-        float nota = 8.5f;
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite o nome: ");
+        String nome = teclado.nextLine();
+
+        System.out.print("Digite a nota: ");
+        float nota = teclado.nextFloat();
+
         System.out.println(String.format("A nota de %s é %.2f", nome, nota));
+
+        teclado.close();
     }
 }
