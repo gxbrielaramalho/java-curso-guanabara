6b3270ee4646eab18fda2fe6ffed197685ef8876
Gabriela Ramalho <gabrielathecreator@gmail.com>
2024-07-09 17:55:28 -0300

    Create CondicionaisEncadeadas.java

---
+++ CondicionaisEncadeadas/CondicionaisEncadeadas.java
@@ -0,0 +1,19 @@
+package CondicionaisEncadeadas;
+
+public class CondicionaisEncadeadas {
+    public static void main(String[] args) {
+        int nota = 65;
+
+        if (nota >= 90) {
+            System.out.println("Aprovado");
+        } else if (nota >= 80) {
+            System.out.println("Aprovado");
+        } else if (nota >= 70) {
+            System.out.println("Aprovado");
+        } else if (nota >= 60) {
+            System.out.println("Reprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+}
